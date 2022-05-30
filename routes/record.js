f9b60f7d26d208097ca190a95979a1c0d48fc169
Hrushikesh Godse <hgodse@emergemarket.com>
2022-05-30 14:33:59 +0530

    Add login

--- routes/record.js
+++ routes/record.js
@@ -25,6 +25,22 @@ recordRoutes.route('/listings').get(async function (_req, res) {
     });
 });
 
+recordRoutes.route('/login').post(async function (_req, res) {
+  const dbConnect = dbo.getDb();
+
+  dbConnect
+    .collection('users')
+    .find({"username": req.body.username, "password": req.body.password })
+    .limit(1)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send('Error fetching listings!');
+      } else {
+        res.json(result);
+      }
+    });
+});
+
 
 // This section will help you get a list of all the records.
 recordRoutes.route('/genre').get(async function (_req, res) {
